5fa2a8689cb47577074dd3dd524665aa11e9579c
cryptozeny <cryptozeny@gmail.com>
2018-04-01 09:27:27 +0900

    테스트넷 제네시스블럭 다시구하는중. 답기다리는데 잠온다: python genesis.py -a SHA256 -z "Anzu Futaba is so kawai!" -t 1522519783 -n 414098458 -b 0x1d00ffff

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -298,9 +298,9 @@ public:
         nDefaultPort = 18444;
         nPruneAfterHeight = 1000;
 
-        genesis = CreateGenesisBlock(1296688602, 2, 0x207fffff, 1, 50 * COIN);
+        genesis = CreateGenesisBlock(1522519783, 3, 0x207fffff, 1, 50 * COIN);
         consensus.hashGenesisBlock = genesis.GetHash();
-        assert(consensus.hashGenesisBlock == uint256S("0x0f9188f13cb7b2c71f2a335e3a4fc328bf5beb436012afca590b1a11466e2206"));
+        assert(consensus.hashGenesisBlock == uint256S("0x29b90b756c626f84f5689eea1783f72c83dffa651f183459856eb01eb6037ca8"));
         assert(genesis.hashMerkleRoot == uint256S("0x9d9fffa7585bc8757d77fe5f3c2f808fb8e5f6b2b93d14fc8d3b52cc14d67b09"));
 
         vFixedSeeds.clear(); //!< Regtest mode doesn't have any fixed seeds.
